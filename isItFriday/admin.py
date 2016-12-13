d42d08189bad1de341de1e0eb941c7569ac20cb9
Sergio Soto <sotonez@gmail.com>
2016-12-13 12:41:14 +0100

    Fixed admin GifDate relation with GifUrl

--- isItFriday/admin.py
+++ isItFriday/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import GifDate, OnlineGif
 
 
-admin.site.register(GifDate)
+class GifDateAdmin(admin.ModelAdmin):
+    filter_horizontal = ('gif_urls',)
+
+
+admin.site.register(GifDate, GifDateAdmin)
 admin.site.register(OnlineGif)
